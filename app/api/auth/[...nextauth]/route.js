59871237aca773e80a19f5e1cf7a4ab908672797
Pranav <pranavkumar192192@gmail.com>
2023-10-16 19:46:35 +0530

    fixed bugs

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,6 @@
 import NextAuth from "next-auth"
 import { AuthOptions } from "@/utils/auth";
 
-export const dynamic = "force-dynamic";
-
 const handler = NextAuth(AuthOptions);
 
 export {handler as GET , handler as POST}
